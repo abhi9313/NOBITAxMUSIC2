0e666f0fb17d7e6a4dcf7a0ab0783eee4c378238
nobitamusic01 <abhaysinghchauhan2062@gmail.com>
2025-06-18 15:01:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abhaysinghchauhan001/NOBITAxMUSIC",
+    "https://github.com/nobitamusic01/NOBITAxMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
